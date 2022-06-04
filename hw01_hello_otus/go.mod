0080ab53b723725caacffec14b12d0e0f1c17514
pabateman <ronofeedback@gmail.com>
2022-06-04 19:58:19 +0700

    Initial hw implementation

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw01_hello_otus
+module github.com/pabateman/otus-golang-hw/hw01_hello_otus
 
 go 1.16
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+)
+
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
 func main() {
-	// Place your code here.
+	input := "Hello, OTUS!"
+
+	fmt.Println(Reverse(input))
 }
