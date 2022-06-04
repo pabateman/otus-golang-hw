d98e535dd2bc5cea6fb789e82fb72f4af38d8619
pabateman <ronofeedback@gmail.com>
2022-06-04 20:04:24 +0700

    import reverse from package and fixed go.mod

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -2,18 +2,11 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/example/stringutil"
 )
-
-func Reverse(s string) string {
-	r := []rune(s)
-	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
-		r[i], r[j] = r[j], r[i]
-	}
-	return string(r)
-}
 
 func main() {
 	input := "Hello, OTUS!"
 
-	fmt.Println(Reverse(input))
+	fmt.Println(stringutil.Reverse(input))
 }
