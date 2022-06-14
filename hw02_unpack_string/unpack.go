7f6d0b2a43eb0e865c5eb21d1a1ced7302218317
pabateman <ronofeedback@gmail.com>
2022-06-15 03:19:13 +0700

    Homework implement

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,98 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+type Kind string
+
+const (
+	KindAsterisk = Kind("ASTERISK")
+	KindDigit    = Kind("DIGIT")
+	KindCommon   = Kind("COMMON")
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func KindOfRune(r rune) Kind {
+	if unicode.IsDigit(r) {
+		return KindDigit
+	}
+	if r == '\\' {
+		return KindAsterisk
+	}
+	return KindCommon
+}
+
+func Unpack(input string) (string, error) {
+	// Обработка исключений
+	if input == "" {
+		return input, nil
+	}
+
+	runedInput := []rune(input)
+	switch KindOfRune(runedInput[0]) {
+	case KindDigit:
+		return "", ErrInvalidString
+	case KindAsterisk, KindCommon:
+	}
+
+	if len(runedInput) == 1 {
+		if KindOfRune(runedInput[0]) == KindAsterisk {
+			return "", ErrInvalidString
+		}
+		return input, nil
+	}
+
+	if KindOfRune(runedInput[len(runedInput)-1]) == KindAsterisk {
+		return "", ErrInvalidString
+	}
+
+	var result strings.Builder
+	var candidate rune
+	var count int
+	var err error
+	i := 0
+
+	for i < len(runedInput) {
+		current := runedInput[i]
+
+		switch KindOfRune(current) {
+		case KindCommon:
+			candidate = current
+		case KindAsterisk:
+			next := runedInput[i+1]
+
+			if KindOfRune(next) == KindCommon {
+				return "", ErrInvalidString
+			}
+			candidate = next
+			i++
+		case KindDigit:
+			return "", ErrInvalidString
+		}
+
+		i++
+		if i > len(runedInput)-1 {
+			result.WriteRune(runedInput[len(runedInput)-1])
+			break
+		}
+
+		current = runedInput[i]
+
+		if KindOfRune(current) == KindDigit {
+			count, err = strconv.Atoi(string(current))
+			if err != nil {
+				return "", err
+			}
+			result.WriteString(strings.Repeat(string(candidate), count))
+			i++
+		} else {
+			result.WriteRune(candidate)
+		}
+	}
+
+	return result.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -15,12 +15,20 @@ func TestUnpack(t *testing.T) {
 		{input: "a4bc2d5e", expected: "aaaabccddddde"},
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
+		{input: "a", expected: "a"},
 		{input: "aaa0b", expected: "aab"},
+		{input: "日本語4", expected: "日本語語語語"},
+		{input: "日本4語", expected: "日本本本本語"},
+		{input: ",4,.m", expected: ",,,,,.m"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "\u65e54\u8a9e", expected: "\u65e5\u65e5\u65e5\u65e5\u8a9e"},
+		{input: "\U000065e5\U00008a9e3", expected: "\U000065e5\U00008a9e\U00008a9e\U00008a9e"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `\3`, expected: `3`},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +42,14 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{
+		"3abc",
+		"45",
+		"aaa10b",
+		`a\aa10\b`,
+		`abccd\`,
+		`\`,
+	}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
